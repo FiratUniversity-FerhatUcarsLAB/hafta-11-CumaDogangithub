3d860da0c9254c3c3921193824e30e9c1f54fa27
Cuma doğan <155235759+CumaDogangithub@users.noreply.github.com>
2025-12-03 20:58:23 +0300

    Enhance comments for method behavior explanations
    Added comments explaining the behavior of methods.

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -1,3 +1,7 @@
+/*Ad: Cuma    
+Soyad: Doğan
+Öğrenci no: 250542003
+ */
 public class Exercise4_4 {
 
     public static int getNumber() {
@@ -12,10 +16,17 @@ public class Exercise4_4 {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
         getNumber();
+        // CEVAP 1: Kod hatasiz calisir 
+        // Metot gorevini yapar ama donen 42 degeri havada kalir 
+        // cunku bir degiskene atamadik veya ekrana yazdirmadik.
+
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
-
-        // Cevaplarinizi yorum olarak ekleyin.
+        
+        // CEVAP 2: Bu satir acilirsa DERLEME HATASI verir 
+        
+        // Cunku 'void' metotlar geriye bir sayi veya deger dondurmez.
+        // Hata: "operator + cannot be applied to void" (
     }
 }
