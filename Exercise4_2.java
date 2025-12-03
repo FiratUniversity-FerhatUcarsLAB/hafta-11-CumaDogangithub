5b81e4960a5a6b94bac4ffa2df13694748a211b5
Cuma doğan <155235759+CumaDogangithub@users.noreply.github.com>
2025-12-03 20:56:39 +0300

    Update Exercise4_2.java

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -1,23 +1,30 @@
+/* Ad: Cuma
+Soyad: Doğan
+Öğrenci no: 250542003
+*/
+
+
+
 public class Exercise4_2 {
 
-  // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
+    // Main her zaman başlangıç noktasıdır (1)
+    public static void main(String[] args) {                
+        zippo("rattle", 13);                                 
+    }                                                       
 
-    public static void main(String[] args) {
-        zippo("rattle", 13);
-    }
+    public static void baffle(String blimp) {               
+        System.out.println(blimp);                          
+        zippo("ping", -5);                                  
+    }                                                       
 
-    public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
-    }
-
-    public static void zippo(String quince, int flag) {
-        if (flag < 0) {
-            System.out.println(quince + " zoop");
-        } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
+   
+    public static void zippo(String quince, int flag) {     
+        if (flag < 0) {                                      
+            System.out.println(quince + " zoop");         
+        } else {                                            
+            System.out.println("ik");                       
+            baffle(quince);                                 
+            System.out.println("boo-wa-ha-ha");              
         }
-    }
+    }                                                       
 }
